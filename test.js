9465b64e12de0b2128aeed300b6da6dd9fb1a07b
Vadym Frolov <vdfroloff@gmail.com>
2022-12-16 16:58:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -266,3 +266,19 @@ textElements.forEach(element => {
 // kick off the render loop
 window.initBurst();
 render();
+
+class classA {
+  private process1 () {
+    return 1;
+  }
+  private process2 () {
+    return 2;
+  }
+  private process3 () {
+    return 3;
+  }
+
+  public superProcess () {
+    return this.process1() + this.process2() + this.process3();
+  }
+}

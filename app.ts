d9494bcdcc71ca474e8b899c102325557c659e9f
Vadym Frolov <vdfroloff@gmail.com>
2023-01-05 20:01:23 +0200

    upds

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 const button = document.querySelector('button');
 const input1 = document.getElementById('num1')! as HTMLInputElement;
 const input2 = document.getElementById('num2')! as HTMLInputElement;
+const input3 = document.getElementById('num3')! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number, num2: number, num3: number) {
+  return num1 + num2 + num3;
 }
 
 button.addEventListener('click', function () {
-  console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value, +input3.value));
 });

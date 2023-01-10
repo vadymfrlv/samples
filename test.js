81e67c8aca4b90b575cb5941ce97df72e118357c
Vadym Frolov <vdfroloff@gmail.com>
2023-01-10 11:46:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,13 @@ function add(num1, num2) {
   return num1 + num2;
 }
 add(1, 1);
+
+function getPromise() {
+  return new Promise(resolve => {
+    resolve(['Text', 50]);
+  });
+}
+
+getPromise().then(data => {
+  console.log(data);
+});
